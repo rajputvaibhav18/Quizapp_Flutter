679792a3e8fa26fd6867a19928f887a261818190
vaibhav <103310689+rajputvaibhav18@users.noreply.github.com>
2023-07-14 22:04:01 +0530

    Create quiz_questions.dart

---
+++ model/quiz_questions.dart
@@ -0,0 +1,13 @@
+class QuizQuestion {
+  const QuizQuestion(this.text, this.answers);
+
+  final String text;
+  final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shuffledlist = List.of(answers);
+    shuffledlist.shuffle();
+
+    return shuffledlist;
+  }
+}

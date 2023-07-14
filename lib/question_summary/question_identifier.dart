c746dcbf7a34dd64d5db9b3aebd8e7edf0410d42
vaibhav <103310689+rajputvaibhav18@users.noreply.github.com>
2023-07-14 22:05:53 +0530

    Create question_identifier.dart

---
+++ lib/question_summary/question_identifier.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class QuestionIdentifier extends StatelessWidget {
+  const QuestionIdentifier({
+    super.key,
+    required this.isCorrectAnswer,
+    required this.questionIndex,
+  });
+
+  final int questionIndex;
+  final bool isCorrectAnswer;
+
+  @override
+  Widget build(BuildContext context) {
+    final questionNumber = questionIndex + 1;
+    return Container(
+      width: 30,
+      height: 30,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: isCorrectAnswer
+            ? Color.fromARGB(255, 47, 160, 21)
+            : Color.fromARGB(255, 245, 10, 10),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Text(
+        questionNumber.toString(),
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Color.fromARGB(255, 22, 2, 56),
+        ),
+      ),
+    );
+  }
+}
